af51659ddf9b4b80f49592e604c08bafb3755297
Vaibhav <vaibhavknw1@gmail.com>
2026-05-09 18:23:09 +0530

    add responsive css and toggle button revert

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     <a href="#hero-section">Home</a><a href="#about-section">About</a><a href="#skills-section">Skills</a>
     <a href="#experience-section">Experience</a><a href="#projects-section">Projects</a><a href="#contact-form">Contact</a>
   </div>
-  <button className="ncta">Hire Me</button>
+  <button className="ncta" >Hire Me</button>
 </nav>
 
   )
